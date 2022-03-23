78d95cf081bed5d3eb72112b7946de7c4955f2af
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-03-23 10:38:59 -0700

    first commit

---
+++ roman_to_int.py
@@ -0,0 +1,68 @@
+'''
+Roman numerals are represented by seven different symbols: I, V, X, L, C, D and M.
+
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, 
+which is simply X + II. The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four 
+is not IIII. Instead, the number four is written as IV. Because the one is before the five we subtract it 
+making four. The same principle applies to the number nine, which is written as IX. There are six instances 
+where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+'''
+
+class Solution(object):
+    def romanToInt(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # Instantiate a numerical value to add or subtract to
+        # This will also be the value that is returned
+        num_val = 0
+        
+        # an assertion error if the string is too long
+        assert 1 <= len(s) <= 15
+        
+        # Instantiate a dictionary that holds the values of each roman numeral
+        # This will be referred to when we want to get a value of a numeral
+        roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        # Iterate through each character in the string
+        for i in range(len(s)):
+            # checking that the character is a valid roman numeral
+            assert s[i] in roman_values.keys()
+            
+            # Start with the last character in the string
+            if i == len(s) - 1:
+                num_val += roman_values[s[i]]
+
+            # check if character value is less than the previous character
+            # this char value = roman_values[str[i]]
+            # last char value = roman_values[str[i + 1]]
+            elif roman_values[s[i]] < roman_values[s[i + 1]]:
+                num_val -= roman_values[s[i]]
+
+            # if this char value is equal to the last or greater than the last, add
+            else:
+                num_val += roman_values[s[i]]
+            
+        # Return the final numerical value
+        return num_val
+
+if __name__ == '__main__':
+    test_object = Solution()
+    test_method = test_object.romanToInt('MCMXCIV')
+    print(test_method)
---
+++ twosum.py
@@ -0,0 +1,38 @@
+'''
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+'''
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        
+        # dictionary with index as key and number as value
+        num_dict = {}
+        for i in range(len(nums)):
+            num_dict[i] = nums[i]
+        
+        i = 0
+        while i in range(len(nums)):
+            # subtract num from target
+            target_num = target - nums[i]
+            # check if result is in dict values
+            if target_num in num_dict.values():
+                # also check that result is not the same as current num
+                if list(num_dict.values()).index(target_num) is not i:
+                    # if conditionals are met, return this num and the index of target num which will be the key
+                    return [i, list(num_dict.values()).index(target_num)]
+            # if nothing found, move onto next num
+            i += 1
+
+if __name__ == '__main__':
+    test_object = Solution()
+    test_method = test_object.twoSum([2,7,11,15], 9)
+    print(test_method)
