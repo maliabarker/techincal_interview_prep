f28f7ac204ddbdb3ec2e87902b5959be778f0408
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-03-29 22:45:22 -0700

    valid parentheses problem

---
+++ valid_parenthese.py
@@ -0,0 +1,83 @@
+'''
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+'''
+'''
+INPUT: string containing only different types of parentheses
+OUTPUT: boolean value indicating if parentheses have matching closing pair in correct order
+*example*
+input: "()[]{}"
+output: true
+input: "(]"
+output: false
+'''
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        # create dictionary that has opening parentheses as key and matching closing as value
+        parentheses = {
+            '(': ')',
+            '[': ']',
+            '{': '}'
+        }
+        # instantiate i as 0
+        i = 0
+        # iterate over each charcter in the given string by 2
+        for i in range(0, len(s), 2):
+            stack = []
+            '''SOLUTION 1'''
+            # check if the amount of characters is odd or even by dividing by two and getting remainder
+                # if remainder is 0 that means its divisible by two and therefore even, which will have a number of characters possible to match
+            if len(s)%2 != 0:
+                return False
+            # check if the next charcter is the dict value of the current character (aka the matching closing character)
+            if s[i+1] != parentheses[s[i]]:
+                # if the next character is not the matching closing charcter OR
+                # if the remainder of the length divided by 2 is more than 0 (meaning there is not enough charcters to make matches)
+                # return false
+                return False
+            '''SOLUTION 2'''
+            # check if tuple is in .items which returns tuples of key value pairs
+            if (s[i], s[i+1]) not in parentheses.items():
+                return False
+            '''SOLUTION 3'''
+            if s[i] in parentheses.keys():
+                if parentheses[s[i]] not in s:
+                    return False
+        # if all characters are checked and pass test, return true
+        return True
+    '''
+    *example*
+    input: "()[]{}"
+    iteration 1:
+        i = 0
+        s[i+1] = s[1] = ')'
+        parentheses[s[0]] = ')'
+        true, continue
+    iteration 2
+        i = 2
+        s[i+1] = s[3] = ']'
+        s[1] = ), parentheses[s[1]] = 
+        
+    '''
+    ################SOLUTION THAT WORKS FOR ALL CASES#######################
+    def isValid2(self, s):
+        parentheses = {'(':')', '{':'}','[':']'}
+        stack = []
+        for char in s:
+            if char in parentheses:  # 1
+                stack.append(char)
+            elif len(stack) == 0 or parentheses[stack.pop()] != char:  # 2
+                return False
+        return len(stack) == 0 # 3
+     
+
+
+if __name__ == '__main__':
+    test_obj = Solution()
+    test_method = test_obj.isValid2("()[{]}")
+    print(test_method)
